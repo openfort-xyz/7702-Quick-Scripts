659b87f996ae47a79eeba2e575f1243c3160a495
0xkoiner <alexgray.bcn.esp@gmail.com>
2025-11-24 18:15:11 +0100

    Init

---
+++ src/data/addressBook.ts
@@ -0,0 +1,41 @@
+// --------------------------- ADDRESS BOOK --------------------------- //
+// This file contains a simple address book for storing and retrieving
+// contact information. It uses a Map to store names and their associated
+// addresses.
+
+import { Hex } from "viem";
+
+type AddressBook = {
+  name: string;
+  version: string;
+  address: Hex;
+  note?: string;
+};
+
+export const addressBook = {
+  entryPointV9: {
+    name: "EntryPoint",
+    version: "v0.9",
+    address: "0x43370900c8de573dB349BEd8DD53b4Ebd3Cce709",
+    note: "ERC-4337 entry point",
+  },
+  opf7702ImplV1: {
+    name: "Openfort 7702 Implementation",
+    version: "v1",
+    address: "0x770200013027B0B3d0151BDeb26757132C95C875",
+    note: "Used for 7702 account deployments",
+  },
+  paymasterV9: {
+    name: "Paymaster",
+    version: "v3Epv9",
+    address: "0x",
+    note: "Used for gas sponsorship",
+  },
+} as const satisfies Record<string, AddressBook>;
+
+export type AddressKey = keyof typeof addressBook;
+
+export const getAddress = (key: AddressKey): Hex => addressBook[key].address;
+
+export const getAddressBook = (key: AddressKey): AddressBook =>
+  addressBook[key];
