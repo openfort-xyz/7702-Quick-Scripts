f11dccff662e0fe48ed9308f5762396a6b4ae5ff
0xkoiner <alexgray.bcn.esp@gmail.com>
2025-11-25 15:44:15 +0100

    test eoaSignature wrapper

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { exit } from "process";
 import { baseSepolia } from "viem/chains";
 import {walletsClient} from "./clients/walletClient";
 import { buildPublicClient } from "./clients/publicClient";
+import { getStubSignature, encodeEOASignature, encodeWebAuthnSignature, encodeP256Signature, WebAuthnSignature, P256Signature } from "./helpers/accountHelpers";
 
 const requireEnv = (name: string): string => {
     const value = process.env[name];
@@ -28,6 +29,9 @@ async function main() {
         throw new Error("walletClientOwner7702 is not configured");
     }
     console.log("Wallet Clients initialized:", ownerWallet.chain);
+
+    const eoaSignature = encodeEOASignature("0x864088609f8bfd27c4648d97ee05a6aac63a4fc0bc018c7123be0cfa530f8ebc394afd61672da9610fbf972c2abc698a7f811eb25595b515e8d5dbbcbf526c631c");
+    console.log("EOA Signature:", eoaSignature);
 }
 
 main().catch((error) => {
