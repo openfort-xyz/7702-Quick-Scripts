c58b57c5779d6a4bb6a499209b03bc541adcbbcb
0xkoiner <alexgray.bcn.esp@gmail.com>
2025-11-25 17:42:27 +0100

    initializeAccount.ts

---
+++ src/helpers/initializeAccount.ts
@@ -0,0 +1,24 @@
+import { IKeys } from "@/interfaces/iTypes";
+import { ABI_7702_ACCOUNT } from "@/data/abis";
+import { encodeFunctionData, Hex } from "viem";
+
+export const initializeCallData = (
+    key: IKeys.IKey,
+    keyDat: IKeys.IKeyReg,
+    sessionKey: IKeys.IKey,
+    sessionKeyData: IKeys.IKeyReg,
+    signature: Hex,
+    initialGuardian: Hex
+): Hex =>
+    encodeFunctionData({
+        abi: ABI_7702_ACCOUNT,
+        functionName: "initialize",
+        args: [
+            key,
+            keyDat,
+            sessionKey,
+            sessionKeyData,
+            signature,
+            initialGuardian
+        ]
+    });
