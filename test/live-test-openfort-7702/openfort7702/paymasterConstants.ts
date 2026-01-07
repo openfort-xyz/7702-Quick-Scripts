afbe1f9a508293ffda4d0c840f0085335d46bb8b
0xKoiner <alexgray.bcn.esp@gmail.com>
2026-01-07 14:39:45 +0100

    (test) attacheAndInit7702.ERC20.ts

--- test/live-test-openfort-7702/openfort7702/paymasterConstants.ts
+++ test/live-test-openfort-7702/openfort7702/paymasterConstants.ts
@@ -1,6 +1,15 @@
-import { Hex, toHex, pad, Address } from "viem";
+import { Hex, toHex, pad, Address, concat, size } from "viem";
 
 export const PaymasterData = {
+    // Sponsored ERC20
+    ERC20_ADDRESS: '0x000e4C20CCC59A2221A5B2E41aD56F4F22eF2202' as Address,
+
+    // Exchange Rate
+    EXCHANGE_RATE: BigInt(30e18),
+
+    // Treasury
+    TREASURY: '0xA84E4F9D72cb37A8276090D3FC50895BD8E5Aaf1' as Address,
+
     // Suffix for async feature in the paymaster Data
     PAYMASTER_SIG_MAGIC: '0x22e325a297439656' as Hex,
 
@@ -23,11 +32,22 @@ export const PaymasterData = {
         return pad(toHex(BUILD_MODE), { size: 1 });
     })(),
 
+    MODE_ERC20: (() => {
+        const MODE_AND_ALLOW_ALL_BUNDLERS_LENGTH = 1n;
+        const BUILD_MODE = (1n << 1n) | MODE_AND_ALLOW_ALL_BUNDLERS_LENGTH;
+        return pad(toHex(BUILD_MODE), { size: 1 });
+    })(),
+
+    // ERC20 Modes
+    // Basic ERC20 mode - no optional fields
+    COMBINED_BYTE_BASIC: '0x00' as Hex,
+
     // Paymaster data timestamp validity windows
     VALID_UNTIL: pad(toHex(1796977534), { size: 6 }),
     VALID_AFTER: pad(toHex(0), { size: 6 }),
 
     // Stub Gas
+    PAYMASTER_VALIDATION_GAS_LIMIT: 100_000n,
     VERIFICATION_GAS_LIMIT: 400_000n,
     POST_GAS_LIMIT: 50_000n,
 } as const;
